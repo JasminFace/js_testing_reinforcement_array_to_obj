9c510bf784d22c8ef7c9753c6f8e4732a73c5ac3
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-02 14:27:02 -0400

    completed assignment

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(array) {
+   var object = {};
+   array.forEach(element => {
+      object[element[0]] = element[1];
+    });
+    return object;  
 }
 
 module.exports = arrayToObj;
